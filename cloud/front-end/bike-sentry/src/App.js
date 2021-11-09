ca653b65cd35210edceb833785cc78c599a1d451
erikd234 <erikd234@gmail.com>
2021-11-08 19:37:41 -0800

    initial project files after create-react-app

---
+++ cloud/front-end/bike-sentry/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to Bike-sentry!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
