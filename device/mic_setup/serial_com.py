ccab0ebdaa21154fbeb8dcec8636623abc36b314
Adam Fong <adamfong888@gmail.com>
2022-03-08 21:08:50 -0800

    Attempting to save wav file
    Arduino UNO is stuck at recording analog read data at 10 bit resolution. Wav file library needs 8 bit or 16 bit. Need a different arduino or ADC which records at the necessary resolution

--- device/mic_setup/serial_com.py
+++ device/mic_setup/serial_com.py
@@ -2,17 +2,19 @@
 Currently, an Arduino is converting the analog input from an electret
 microphone and sending each sample to the serial port 
 '''
-#%%
+
 import serial.tools.list_ports as list_ports
 import serial
 import re
+import scipy.io.wavfile as wav
+import numpy as np
 
 def active_ports():
     ports = list(list_ports.comports())
     for p in ports:
         print(p)
 
-def get_serial_data(br = 9600, duration = 10, f = 17e3):
+def get_serial_data(br = 9600, duration = 1, f = 18200):
     '''
     Gets serial data for the defined duration of time. Default is 10 seconds at 17 kHz frequency
     '''
@@ -30,7 +32,7 @@ def get_serial_data(br = 9600, duration = 10, f = 17e3):
     data = []
     
     # continue grabbing data until d 
-    while(counter <= f):
+    while(counter <= d):
         try:
             val = s.readline().decode("cp1252")
             counter += 1
@@ -39,9 +41,18 @@ def get_serial_data(br = 9600, duration = 10, f = 17e3):
         except:
             continue                
 
-    return data
+    return np.array(data).astype(int)
 
 if __name__ == "__main__":
     active_ports() 
-    d = get_serial_data(br = 2000000)
+    d = get_serial_data(br = 38400)
     print(len(d))
+    print(type(d))
+    print(d)
+    
+    # setup
+    fs = 18200
+
+    # save .wav
+    filename = "testing_electret.wav"
+    wav.write(filename, fs, d)
